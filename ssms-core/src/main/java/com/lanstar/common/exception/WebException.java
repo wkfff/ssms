c59beddba29a62f295475064ac940f5ae2009b8d
Zgray <cnzgray@qq.com>
2015-04-02 10:53:37 +0800

    添加系统数据库配置信息与异常基类

---
+++ ssms-core/src/main/java/com/lanstar/common/exception/WebException.java
@@ -0,0 +1,30 @@
+/*
+ * 项目名称：安全生产标准化管理系统(Safety Standardization Management System)
+ * 版权申明：福州市磬基电子有限公司、福州市蓝石电子有限公司所有，未经许可不得在任何软件中以任何形式使用全部或部分代码，不得更改本项目的代码。
+ * 文件名称：WebException.java
+ * 创建时间：2015-04-01
+ * 创建用户：张铮彬
+ */
+
+package com.lanstar.common.exception;
+
+public class WebException extends RuntimeException {
+    public WebException() {
+    }
+
+    public WebException(String message) {
+        super(message);
+    }
+
+    public WebException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public WebException(Throwable cause) {
+        super(cause);
+    }
+
+    public WebException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
