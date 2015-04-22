dcd7752d0644930628326b9ae7fc0340768456f7
Zgray <cnzgray@qq.com>
2015-04-22 11:41:39 +0800

    加入其它数据

--- scripts/11_initParaData.sql
+++ scripts/11_initParaData.sql
@@ -19,7 +19,6 @@ VALUES
 INSERT INTO `SYS_STDTMP_FOLDER` (`SID`, `C_NAME`, `R_SID`, `C_DESC`, `C_LOGO`, `P_PROFESSION`, `S_PROFESSION`, `R_CREATE`, `S_CREATE`, `T_CREATE`, `R_UPDATE`, `S_UPDATE`, `T_UPDATE`, `R_TANENT`, `S_TANENT`, `P_TANENT`, `N_STATE`, `B_DELETE`, `N_INDEX`, `N_VERSION`)
 VALUES
     (1, '工贸达标体系模板', 0, 'adfdfe', NULL, '01', '工贸', 0, 'admin', '2015-04-20 11:41:52', 0, 'admin', '2015-04-21 22:35:54', 0, 'admin', 'S', NULL, NULL, NULL, NULL),
-    (5, '', 0, '', NULL, NULL, NULL, 0, 'admin', '2015-04-21 15:22:27', NULL, NULL, '2015-04-21 15:22:27', 0, 'admin', 'S', NULL, NULL, NULL, NULL),
     (4, '安全生产目标', 1, '安全生产目标', NULL, '01', '工贸', 0, 'admin', '2015-04-21 10:36:28', 0, 'admin', '2015-04-21 22:36:36', 0, 'admin', 'S', NULL, NULL, NULL, NULL),
     (7, '目标', 4, '', NULL, '01', '工贸', 0, 'admin', '2015-04-21 22:36:52', NULL, NULL, '2015-04-21 22:36:52', 0, 'admin', 'S', NULL, NULL, NULL, NULL),
     (8, '监测与考核', 4, '', NULL, '01', '工贸', 0, 'admin', '2015-04-21 22:37:09', NULL, NULL, '2015-04-21 22:37:09', 0, 'admin', 'S', NULL, NULL, NULL, NULL),
@@ -36,8 +35,16 @@ VALUES
     (19, '规章制度', 11, '', NULL, '01', '工贸', 0, 'admin', '2015-04-21 22:52:57', NULL, NULL, '2015-04-21 22:52:57', 0, 'admin', 'S', NULL, NULL, NULL, NULL),
     (20, '操作规程', 11, '', NULL, '01', '工贸', 0, 'admin', '2015-04-21 22:53:05', NULL, NULL, '2015-04-21 22:53:05', 0, 'admin', 'S', NULL, NULL, NULL, NULL),
     (21, '评估', 11, '', NULL, '01', '工贸', 0, 'admin', '2015-04-21 22:53:14', NULL, NULL, '2015-04-21 22:53:14', 0, 'admin', 'S', NULL, NULL, NULL, NULL),
-    (22, '文件和档案管理', 11, '', NULL, '01', '工贸', 0, 'admin', '2015-04-21 22:53:23', NULL, NULL, '2015-04-21 22:53:23', 0, 'admin', 'S', NULL, NULL, NULL, NULL);
-
+    (22, '文件和档案管理', 11, '', NULL, '01', '工贸', 0, 'admin', '2015-04-21 22:53:23', NULL, NULL, '2015-04-21 22:53:23', 0, 'admin', 'S', NULL, NULL, NULL, NULL),
+    (23, '教育培训管理', 12, '', NULL, '01', '工贸', 0, 'admin', '2015-04-22 10:36:55', NULL, NULL, '2015-04-22 10:36:55', 0, 'admin', 'S', NULL, NULL, NULL, NULL),
+    (24, '安全生产管理人员教育培训', 12, '', NULL, '01', '工贸', 0, 'admin', '2015-04-22 10:37:12', NULL, NULL, '2015-04-22 10:37:12', 0, 'admin', 'S', NULL, NULL, NULL, NULL),
+    (25, '操作岗位人员教育培训', 12, '', NULL, '01', '工贸', 0, 'admin', '2015-04-22 10:39:10', NULL, NULL, '2015-04-22 10:39:10', 0, 'admin', 'S', NULL, NULL, NULL, NULL),
+    (26, '特种作业人员教育培训', 12, '', NULL, '01', '工贸', 0, 'admin', '2015-04-22 10:39:25', NULL, NULL, '2015-04-22 10:39:25', 0, 'admin', 'S', NULL, NULL, NULL, NULL),
+    (27, '其他人员教育培训', 12, '', NULL, '01', '工贸', 0, 'admin', '2015-04-22 10:39:35', NULL, NULL, '2015-04-22 10:39:35', 0, 'admin', 'S', NULL, NULL, NULL, NULL),
+    (28, '安全文化建设', 12, '', NULL, '01', '工贸', 0, 'admin', '2015-04-22 10:39:46', NULL, NULL, '2015-04-22 10:39:46', 0, 'admin', 'S', NULL, NULL, NULL, NULL),
+    (29, '生产设备设施建设', 13, '', NULL, '01', '工贸', 0, 'admin', '2015-04-22 10:40:04', NULL, NULL, '2015-04-22 10:40:04', 0, 'admin', 'S', NULL, NULL, NULL, NULL),
+    (30, '设备设施运行管理', 13, '', NULL, '01', '工贸', 0, 'admin', '2015-04-22 10:40:18', NULL, NULL, '2015-04-22 10:40:18', 0, 'admin', 'S', NULL, NULL, NULL, NULL),
+    (31, '设备设施到货验收和报废拆除', 13, '', NULL, '01', '工贸', 0, 'admin', '2015-04-22 10:57:20', NULL, NULL, '2015-04-22 10:57:20', 0, 'admin', 'S', NULL, NULL, NULL, NULL);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
