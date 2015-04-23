b6590c1d7e34d2f33216864e966d7072b2497b3b
zlsu <645358999@qq.com>
2015-04-23 16:05:44 +0800

    达标体系培训文件模板

---
+++ ssms-webapp/src/main/java/com/lanstar/controller/sys/stdtmp_file_05Validator.java
@@ -0,0 +1,30 @@
+/*
+ * 项目名称：安全生产标准化管理系统(Safety Standardization Management System)
+ * 版权申明：福州市磬基电子有限公司、福州市蓝石电子有限公司所有，未经许可不得在任何软件中以任何形式使用全部或部分代码，不得更改本项目的代码。
+ * 文件名称：stdtmp_file_05Validator.java
+ * 创建时间：下午3:53:19
+ * 创建用户：苏志亮
+ */
+package com.lanstar.controller.sys;
+
+import com.lanstar.controller.ActionValidator;
+import com.lanstar.core.handle.HandlerContext;
+
+/**
+ * @author Administrator
+ *
+ */
+public class stdtmp_file_05Validator extends ActionValidator {
+
+    @Override
+    protected void validate( HandlerContext c ) {
+        this.validateRequiredString( "", "", "" );
+
+    }
+
+    @Override
+    protected void handleError( HandlerContext c ) {
+        c.setValue( "_ERROR_", this.errorMsg );
+    }
+
+}
