0da29db685a2d953cbdc05105d35a3ee8903ff53
Zgray <cnzgray@qq.com>
2015-04-26 12:13:51 +0800

    添加typescript编译器支持

---
+++ ssms-webapp/src/Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        watch: {
+            typescript: {
+                files: ['main/webapp/typescript/**/*.ts'],
+                tasks: ['ts', 'copy'],
+                options: {
+                    spawn: false
+                }
+            }
+        },
+        copy: {
+            copyTSMap: {
+                cwd: 'dist',
+                src: '*.js.map',
+                dest: 'main/webapp/typescript/maps/',
+                filter: 'isFile',
+                expand: true
+            },
+            copyJs: {
+                cwd: 'dist',
+                src: '*.js',
+                dest: 'main/webapp/resource/js/',
+                filter: 'isFile',
+                expand: true
+            }
+        },
+        ts: {
+            build: {
+                //When abc.ts is compiled to abc.js, it will reference /maps/abc.js.map
+                src: ['main/webapp/typescript/**/*.ts', 'typings/tsd.d.ts'],
+                outDir: 'dist',
+                options: {
+                    mapRoot: "/typescript/maps",
+                    sourceRoot: "/typescript",
+                    target: "es3", //for IE8 and below "es5" (default) | "es3" | "es6",
+                    verbose: true, //false (default) | true
+                    comments: true, //preserves comments in output. true | false (default)
+                    declaration: true, // true | false (default)
+                    module: "amd", // "amd" (default) | "commonjs" | ""
+                    sourceMap: true, // true (default) | false
+                    fast: 'never'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-ts');
+
+    grunt.event.on('watch', function (action, filepath) {
+        grunt.config('ts.build.src', [filepath, 'typings/tsd.d.ts']);
+    });
+
+    grunt.registerTask('dev', ['ts', 'copy']);
+    grunt.registerTask('default', ['watch']);
+};
