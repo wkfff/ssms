b0da68738526e9dfc91d0306488b64f384628370
Zgray <cnzgray@qq.com>
2015-05-05 20:54:11 +0800

    添加些工具方法

--- ssms-webapp/src/main/webapp/resource/js/core.js
+++ ssms-webapp/src/main/webapp/resource/js/core.js
@@ -8,4 +8,17 @@ String.prototype.format = function () {
 String.prototype.replaceAll = function (searchValue, replaceValue) {
     return this.replace(new RegExp(searchValue, "gm"), replaceValue);
 };
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function (searchString, position) {
+        position = position || 0;
+        return this.lastIndexOf(searchString, position) === position;
+    };
+}
+var utils;
+(function (utils) {
+    function uuid() {
+        return "_" + new Date().valueOf();
+    }
+    utils.uuid = uuid;
+})(utils || (utils = {}));
 //# sourceMappingURL=/typescript/maps/core.js.map
