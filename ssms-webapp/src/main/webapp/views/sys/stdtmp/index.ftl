444e5ce1a292c2ef874323048c417db57646167f
Zgray <cnzgray@qq.com>
2015-05-08 14:42:34 +0800

    模板管理页面调整

--- ssms-webapp/src/main/webapp/views/sys/stdtmp/index.ftl
+++ ssms-webapp/src/main/webapp/views/sys/stdtmp/index.ftl
@@ -2,61 +2,50 @@
 <#assign script>
 <script type="text/javascript" src="index.js"></script>
 <script type="text/javascript">
-    ko.applyBindings(new viewModel({template: ${R_SID}}));
+    ko.applyBindings(new ViewModel(${R_SID}));
 </script>
 </#assign>
 <@layout.doLayout script=script>
 <div class="easyui-layout" data-options="fit:true">
     <div data-options="region:'west', title:'功能导航', split:true" style="width: 200px">
-        <ul data-bind="tree: tree"></ul>
+        <ul data-bind="treeValue:selectedNode,easyuiOptions:treeSettings"></ul>
     </div>
     <div data-options="region:'center', border:false">
         <div class="easyui-tabs" fit="true">
             <div title="概要" iconCls="icon-details">
                 <div class="z-toolbar">
-                    <a href="#" class="easyui-linkbutton" plain="true" iconCls="icon-save" data-bind="click: rec.saveClick">保存</a>
-                    <a href="#" class="easyui-linkbutton" plain="true" iconCls="icon-cancel" data-bind="click: rec.removeClick">删除</a>
+                    <a href="#" class="easyui-linkbutton" plain="true" iconCls="icon-save" data-bind="click: recEvents.saveClick">保存</a>
+                    <a href="#" class="easyui-linkbutton" plain="true" iconCls="icon-cancel" data-bind="click: recEvents.removeClick">删除</a>
                 </div>
-                <form class="form" method="post" data-bind="form: rec, formValue: rec.data">
+                <form class="form" method="post" data-bind="with:rec">
                     <p class="ue-clear">
                         <label>名称：</label>
                         <span class="control">
-                            <input class="easyui-textbox" type="text" name="C_NAME"/>
+                            <input data-bind="textboxValue:C_NAME"/>
                         </span>
                     </p>
 
                     <p class="ue-clear">
                         <label>描述：</label>
                         <span class="control">
-                            <input class="easyui-textbox" type="text" name="C_DESC" multiline="true"/>
+                            <input data-bind="textareaValue:C_DESC"/>
                         </span>
                     </p>
-                    <input type="hidden" name="SID"/>
-                    <input type="hidden" name="R_TEMPLATE" value="${R_SID}"/>
                 </form>
             </div>
-            <div title="子分类" iconCls="icon-table" data-bind="with: items">
-                <div id="items_toolbar" class="z-toolbar">
-                    <a href="#" class="easyui-linkbutton" iconCls="icon-reload" plain="true" data-bind="click:refreshClick">刷新</a>
-                    <a href="#" class="easyui-linkbutton" iconCls="icon-add" plain="true" data-bind="click: addClick">添加</a>
-                    <a href="#" class="easyui-linkbutton" iconCls="icon-edit" plain="true" data-bind="click: editClick">编辑</a>
-                    <a href="#" class="easyui-linkbutton" iconCls="icon-remove" plain="true" data-bind="click: deleteClick">删除</a>
-                    <a href="#" class="easyui-linkbutton" iconCls="icon-save" plain="true" data-bind="click:saveClick">保存</a>
+            <div title="子分类" iconCls="icon-table">
+                <div id="toolbar" class="z-toolbar">
+                    <a href="#" class="easyui-linkbutton" iconCls="icon-reload" plain="true" data-bind="click: gridEvents.refreshClick">刷新</a>
+                    <a href="#" class="easyui-linkbutton" iconCls="icon-add" plain="true" data-bind="click: gridEvents.addClick">添加</a>
+                    <a href="#" class="easyui-linkbutton" iconCls="icon-edit" plain="true" data-bind="click: gridEvents.editClick">编辑</a>
+                    <a href="#" class="easyui-linkbutton" iconCls="icon-remove" plain="true" data-bind="click: gridEvents.deleteClick">删除</a>
+                    <a href="#" class="easyui-linkbutton" iconCls="icon-save" plain="true" data-bind="click: gridEvents.saveClick">保存</a>
                 </div>
-                <table data-bind="datagrid: $data">
-                    <thead>
-                    <tr>
-                        <th field="C_NAME" width="100" editor="{ type:'validatebox', options:{ required:true, validType:['length[0, 300]'] }}">名称</th>
-                        <th field="C_DESC" width="200" editor="{ type:'textarea', options:{ validType:['length[0, 1000]'] }}">描述</th>
-                        <th field="N_INDEX" width="80" align="center" editor="{ type:'numberbox', options: {required:true} }">排序</th>
-                    </tr>
-                    </thead>
-                </table>
+                <table data-bind="datagridValue:selectItem,datagridEditValue:editItem,easyuiOptions:gridSettings"></table>
             </div>
             <div title="文件" iconCls="icon-table" style="padding: 2px">
                 <iframe id="iframe" frameborder="0" width="100%" height="99%" data-bind="attr: {src: fileUrl}"></iframe>
             </div>
         </div>
     </div>
-</div>
 </@>
--- ssms-webapp/src/main/webapp/views/sys/stdtmp/index.js
+++ ssms-webapp/src/main/webapp/views/sys/stdtmp/index.js
@@ -5,137 +5,153 @@
  * 创建时间：2015-05-06
  * 创建用户：张铮彬
  */
-
-function viewModel(queryParams) {
+function ViewModel(templateId) {
     var self = this;
-    self.sid = ko.observable();
-    self.selectNode = ko.observable();
-    // 值改变的时候重新执行的行为
-    self.sid.subscribe(function (newValue) {
-        if (!newValue) return;
-        self.rec.load();
-        self.items.queryParams({R_SID: newValue});
-        /*listChildren.load();
-         listFile.load();*/
-    });
 
-    self.tree = {
-        url: 'tree.json',
-        queryParams: queryParams
-    };
-    self.tree.onSelect = function (node) {
-        self.sid(node.id);
-        self.selectNode(node);
-    };
-    self.tree.onLoadSuccess = function () {
-        var node = self.selectNode();
-        if (node) node = self.tree.tree('find', self.sid());
-        if (!node) node = self.tree.tree('getRoot');
-        self.sid(null);
-        self.tree.tree('select', node.target);
-    };
-    self.tree.reload = function () {
-        self.tree.tree('reload');
-    };
-
-    self.rec = {
-        data: ko.observable({})
-    };
-    self.rec.load = function () {
-        $.post('rec.json', {sid: self.sid()}, function (result) {
-            self.rec.data(result);
-        });
-    };
-    self.rec.saveClick = function () {
-        self.rec.form('submit', {
-            url: 'save.do',
-            onSubmit: function () {
-                return self.rec.form('validate');
-            },
-            success: function () {
-                $.messager.alert('提示信息', '保存成功!', 'info', function () {
-                    self.tree.reload();
-                });
-            }
-        });
-    };
-    self.rec.removeClick = function () {
-        $.post('del.do', {sid: self.sid}, function () {
-            self.tree.reload();
+    var model = {
+        selectedNode: ko.observable(),
+        selectItem: ko.observable(),
+        selectIndex: ko.pureComputed(function () {
+            var row = model.selectItem();
+            if (row) return settings.gridSettings.datagrid('getRowIndex', row);
+        }),
+        editItem: ko.observable(),
+        sid: ko.observable(),
+        rec: ko.mapping.fromJS({
+            C_NAME: null,
+            C_DESC: null,
+            SID: null,
+            R_TEMPLATE: templateId
+        }),
+        fileUrl: ko.pureComputed(function () {
+            var sid = model.sid();
+            if (!sid) return;
+            return '/sys/stdtmp_file/index.html?R_SID={0}'.format(sid);
         })
     };
+    model.selectedNode.subscribe(function (newValue) {
+        if (!newValue) return;
+        model.sid(newValue.id);
+        if (settings.gridSettings.datagrid)
+            settings.gridSettings.datagrid({
+                url: "list.json",
+                queryParams: {R_SID: newValue.id}
+            })
+    });
+    model.sid.subscribe(function (newValue) {
+        $.post('rec.json', {sid: model.sid()}, function (result) {
+            ko.mapping.fromJS(result, model.rec);
+        });
+    });
 
-    self.items = {
-        url: "list.json",
-        queryParams: ko.observable(),
-        toolbar: '#items_toolbar'
-    };
-    self.items.onBeforeLoad = function () {
-        return self.items.queryParams() != null;
-    };
-    self.items.refreshClick = function () {
-        self.items.datagrid('reload');
-    };
-    self.items.addClick = function () {
-        if (self.items.onClickRow()) {
-            var row = {SID: utils.uuid(), R_SID: self.sid()};
-            self.items.datagrid('appendRow', row);
-            self.items.datagrid('selectRecord', row.SID);
-            self.items.editClick();
-        }
-    };
-    self.items.editClick = function () {
-        var row = self.items.datagrid('getSelected');
-        if (row) {
-            var index = self.items.datagrid('getRowIndex', row);
-            self.items.datagrid('beginEdit', index);
-            self.items.edit_id = row.SID;
-        }
-    };
-    self.items.deleteClick = function () {
-        var row = self.items.datagrid('getSelected');
-        if (row) {
-            var index = self.items.datagrid('getRowIndex', row);
-            self.items.datagrid('deleteRow', index);
-            if (!row.SID.toString().startsWith("_")) $.post('del.do', {sid: row.SID}, function () {
-                $.messager.alert('消息', '成功删除记录！');
-            });
-        }
-    };
-    self.items.saveClick = function () {
-        if (!self.items.onClickRow()) {
-            self.items.deleteClick();
-            return;
-        }
-        var changes = self.items.datagrid('getChanges');
-        if (changes.length > 0) {
-            $.post("batchSave.do", {data: $.toJSON(changes)}, function () {
-                $.messager.alert('消息', '成功保存记录！', "info", function () {
-                    self.tree.reload();
-                });
-            });
-        }
-    };
-    self.items.onDblClickRow = self.items.editClick;
-    self.items.onClickRow = function () {
-        var edit_id = self.items.edit_id;
-        if (!!edit_id) {
-            var index = self.items.datagrid('getRowIndex', edit_id);
-            if (self.items.datagrid('validateRow', index)) { //通过验证
-                self.items.datagrid('endEdit', index);
-                self.items.edit_id = undefined;
+    var settings = {
+        treeSettings: {
+            url: 'tree.json',
+            queryParams: {template: templateId},
+            onLoadSuccess: function () {
+                var node = settings.treeSettings.tree('getRoot');
+                if (node.id !== model.sid()) {
+                    node = settings.treeSettings.tree('find', model.sid()) || node;
+                }
+                model.selectedNode(node);
             }
-            else { //未通过验证
-                self.items.datagrid('selectRecord', edit_id);
-                return false;
-            }
+        },
+        gridSettings: {
+            idField: 'SID',
+            rownumbers: true,
+            pagination: true,
+            fit: true,
+            toolbar: '#toolbar',
+            columns: [
+                [
+                    {
+                        field: 'C_NAME',
+                        title: '名称',
+                        width: 100,
+                        editor: {type: 'validatebox', options: {required: true, validType: ['length[0, 300]']}}
+                    },
+                    {
+                        field: 'C_DESC',
+                        title: '描述',
+                        width: 200,
+                        editor: {type: 'textarea', options: {validType: ['length[0, 1000]']}}
+                    },
+                    {
+                        field: 'N_INDEX',
+                        title: '排序',
+                        width: 80,
+                        align: "center",
+                        editor: {type: 'numberbox', options: {required: true}}
+                    }
+                ]
+            ]
         }
-        return true;
     };
 
-    self.fileUrl = ko.pureComputed(function () {
-        var sid = self.sid();
-        if (!sid) return;
-        return '/sys/stdtmp_file/index.html?R_SID={0}'.format(self.sid());
-    })
+    var events = {
+        recEvents: {
+            saveClick: function () {
+                if ($form.validate($('.form'))) {
+                    var value = ko.mapping.toJS(model.rec);
+                    $.post('save.do', value, function (result) {
+                        if (result.SID)
+                            $.messager.alert("提示", "保存成功", "info", function () {
+                                settings.treeSettings.tree('reload');
+                            });
+                        else {
+                            $.messager.alert("提示", "保存失败", "warning");
+                        }
+                    }, "json")
+                }
+            },
+            removeClick: function () {
+                $.post('del.do', {sid: model.sid()}, function () {
+                    settings.treeSettings.tree('reload');
+                })
+            }
+        },
+        gridEvents: {
+            refreshClick: function () {
+                settings.gridSettings.datagrid('reload');
+            }, addClick: function () {
+                if (settings.gridSettings.datagrid('validateRow', model.selectIndex())) {
+                    var row = {SID: utils.uuid(), R_SID: model.sid(), R_TEMPLATE: templateId};
+                    settings.gridSettings.datagrid('appendRow', row);
+                    model.editItem(row);
+                }
+            }, editClick: function () {
+                model.editItem(model.selectItem());
+            }, deleteClick: function () {
+                var row = model.selectItem();
+                if (row) {
+                    if (!row.SID.toString().startsWith("_"))
+                        $.post('del.do', {sid: row.SID}, function () {
+                            $.messager.alert('消息', '成功删除记录！', 'info', function () {
+                                settings.gridSettings.datagrid('reload');
+                            });
+                        });
+                    else {
+                        settings.gridSettings.datagrid('deleteRow', model.selectIndex());
+                    }
+                }
+            }, saveClick: function () {
+                model.editItem(null);
+                if (model.editItem()) {
+                    $.messager.alert('警告', '当前编辑行数据不正确', 'warning');
+                    return;
+                }
+                var changes = settings.gridSettings.datagrid('getChanges');
+                if (changes.length > 0) {
+                    $.post("batchSave.do", {data: $.toJSON(changes)}, function () {
+                        $.messager.alert('消息', '成功保存记录！', "info", function () {
+                            settings.treeSettings.tree('reload');
+                        });
+                    });
+                }
+            }
+        }
+    };
+
+    $.extend(self, model, settings, events);
 }
+
