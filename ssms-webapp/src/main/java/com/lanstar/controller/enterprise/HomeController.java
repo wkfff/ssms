5c006c045100f3170e40438a11bdc194b9d0bf1e
xiuj <xiuj.lee@gmail.com>
2015-06-14 09:48:32 +0800

    首页通知公告显示。

--- ssms-webapp/src/main/java/com/lanstar/controller/enterprise/HomeController.java
+++ ssms-webapp/src/main/java/com/lanstar/controller/enterprise/HomeController.java
@@ -11,53 +11,59 @@ package com.lanstar.controller.enterprise;
 import com.lanstar.app.Const;
 import com.lanstar.core.Controller;
 import com.lanstar.identity.IdentityContext;
+import com.lanstar.model.system.Notice;
 import com.lanstar.model.system.Profession;
 import com.lanstar.model.tenant.TemplateFolder;
+import com.lanstar.plugin.activerecord.Db;
 import com.lanstar.service.enterprise.EnterpriseService;
 
 import java.util.List;
 
 public class HomeController extends Controller {
     public void index() {
-        IdentityContext identityContext = IdentityContext.getIdentityContext( this );
+        IdentityContext identityContext = IdentityContext.getIdentityContext(this);
         EnterpriseService enterpriseService = identityContext.getEnterpriseService();
         boolean needChooseProfessions = enterpriseService.getProfessionService() == null;
-        if ( needChooseProfessions ) {
+        if (needChooseProfessions) {
             List<Profession> professions = enterpriseService.getProfessions();
-            if ( professions.size() == 1 ) {
-                setAttr( "profession", professions.get( 0 ).getInt( "SID" ) );
+            if (professions.size() == 1) {
+                setAttr("profession", professions.get(0).getInt("SID"));
                 setTemplate();
-                removeAttr( "profession" );
+                removeAttr("profession");
                 needChooseProfessions = false;
             }
         }
 
-        setSessionAttr( "needChooseProfessions", needChooseProfessions );
+        setSessionAttr("needChooseProfessions", needChooseProfessions);
 
-        if ( needChooseProfessions == false ) setAttr( "profession", enterpriseService.getProfessionService() );
+        if (needChooseProfessions == false)
+            setAttr("profession", enterpriseService.getProfessionService());
 
-        setAttr( Const.HOME_PAGE, "/e/home" );
+        setAttr(Const.HOME_PAGE, "/e/home");
     }
 
     public void setTemplate() {
-        Integer id = getAttr( "profession" );
-        if ( id == null ) id = getParaToInt( "profession" );
-        Profession profession = Profession.dao.findById( id );
-        IdentityContext context = IdentityContext.getIdentityContext( this );
-        context.getEnterpriseService().setProfessionService( profession );
+        Integer id = getAttr("profession");
+        if (id == null)
+            id = getParaToInt("profession");
+        Profession profession = Profession.dao.findById(id);
+        IdentityContext context = IdentityContext.getIdentityContext(this);
+        context.getEnterpriseService().setProfessionService(profession);
     }
 
     public void getProfessions() {
-        renderJson( IdentityContext.getIdentityContext( this ).getEnterpriseService().getProfessions() );
+        renderJson(IdentityContext.getIdentityContext(this).getEnterpriseService().getProfessions());
     }
 
     public void home() {
-        IdentityContext identityContext = IdentityContext.getIdentityContext( this );
-        TemplateFolder folder = identityContext.getEnterpriseService()
-                                                             .getProfessionService()
-                                                             .getTenantTemplateFolder();
-        setAttr( "FILE_COUNT", folder.getFileCount() );
+        IdentityContext identityContext = IdentityContext.getIdentityContext(this);
+        TemplateFolder folder = identityContext.getEnterpriseService().getProfessionService().getTenantTemplateFolder();
+        setAttr("FILE_COUNT", folder.getFileCount());
 
-        setAttr( "FILE_NO_CREATE", identityContext.getTenantDb().queryLong( "select COUNT(*) from ssm_stdtmp_file where N_COUNT = 0" ) );
+        setAttr("FILE_NO_CREATE",
+                identityContext.getTenantDb().queryLong("select COUNT(*) from ssm_stdtmp_file where N_COUNT = 0"));
+
+        List<Notice> rs_notice = Notice.dao.find("select * from ssm_notice");
+        setAttr("rs_notice", rs_notice );
     }
 }
