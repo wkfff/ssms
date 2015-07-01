524ff2175b8b37eccc4ff5de516919541e950d5a
Zgray <cnzgray@qq.com>
2015-07-01 11:32:17 +0800

    提交首页变跟

--- ssms-webapp/src/main/java/com/lanstar/controller/enterprise/HomeController.java
+++ ssms-webapp/src/main/java/com/lanstar/controller/enterprise/HomeController.java
@@ -17,6 +17,8 @@ import com.lanstar.model.system.Profession;
 import com.lanstar.model.tenant.*;
 import com.lanstar.service.enterprise.EnterpriseService;
 import com.lanstar.service.enterprise.ProfessionService;
+import com.lanstar.service.enterprise.TemplateInitTask;
+import com.lanstar.service.enterprise.TemplateInitializerState;
 
 import java.util.List;
 import java.util.Objects;
@@ -26,7 +28,7 @@ public class HomeController extends Controller {
         IdentityContext identityContext = IdentityContext.getIdentityContext( this );
         EnterpriseService enterpriseService = identityContext.getEnterpriseService();
         // 如果专业服务还没初始化，那么就初始化服务。
-        if ( enterpriseService.getProfessionService() == null ) {
+        if ( enterpriseService.professionServiceInitialized() == false ) {
             List<Profession> professions = enterpriseService.getProfessions();
             // 先从cookie里取出，如果没有的话就直接设置第一个了。
             Integer professionId = getCookieToInt( "profession" );
@@ -42,18 +44,10 @@ public class HomeController extends Controller {
                 // 如果根据cookie的值还是没有找到专业的话，则默认为第一个专业。
                 if ( currentProfession == null ) currentProfession = professions.get( 0 );
             }
-            setProfession( currentProfession );
+            if ( setProfession( currentProfession ) == false ) return;
         }
 
         setAttr( Const.HOME_PAGE, "/e/home" );
-    }
-
-    public void setTemplate() {
-        Integer id = getParaToInt( "profession" );
-        Profession profession = Profession.dao.findById( id );
-        setProfession( profession );
-
-        redirect( "/" );
     }
 
     public void home() {
@@ -70,27 +64,64 @@ public class HomeController extends Controller {
                                                           professionService.getSystemTemplate().getId(),
                                                           professionService.getId() ) );
 
+        // TODO 从待办表读取待办
         List<TemplateFile> files = TemplateFile.dao.find( "SELECT A.*\n"
                 + "FROM SSM_STDTMP_FILE A\n"
                 + "inner join SSM_STDTMP_FOLDER B on a.r_sid = b.sid\n"
                 + "where B.R_TEMPLATE = ? AND B.R_TENANT=? AND B.P_TENANT=?\n"
                 + "and A.N_COUNT = 0 ORDER BY B.N_INDEX, B.SID, A.N_INDEX, SID\n"
-                + "limit 0, 15", professionService.getSystemTemplate()
-                                                  .getId(), identityContext.getTenantId(), TenantType.ENTERPRISE
+                + "limit 10", professionService.getSystemTemplate()
+                                               .getId(), identityContext.getTenantId(), TenantType.ENTERPRISE
                 .getName() );
         setAttr( "rs_todo", files );
 
         List<Notice> rs_notice = Notice.dao.find( "select * from ssm_notice" );
         setAttr( "rs_notice", rs_notice );
 
-        setAttr( "rs_dev", TemplateFile08.dao.find( "select * from ssm_stdtmp_file_08 limit 15" ) );
-        setAttr( "rs_yh", TemplateFile06.dao.find( "select * from ssm_stdtmp_file_06 limit 15" ) );
-        setAttr( "rs_ry", TemplateFile07.dao.find( "select * from SSM_STDTMP_FILE_07 limit 15" ) );
+        setAttr( "rs_dev", TemplateFile08.dao.find( "select * from ssm_stdtmp_file_08 limit 10" ) );
+        setAttr( "rs_yh", TemplateFile06.dao.find( "select * from ssm_stdtmp_file_06 limit 10" ) );
+        setAttr( "rs_ry", TemplateFile07.dao.find( "select * from SSM_STDTMP_FILE_07 limit 10" ) );
     }
 
-    private void setProfession( Profession profession ) {
+    public void setTemplate() {
+        Integer id = getParaToInt( "profession" );
+        Profession profession = Profession.dao.findById( id );
+        if ( setProfession( profession ) ) redirect( "/" );
+    }
+
+    public void status() {
         IdentityContext context = IdentityContext.getIdentityContext( this );
-        context.getEnterpriseService().setProfessionService( profession );
+        ProfessionService professionService = context.getEnterpriseService().getProfessionService();
+        TemplateInitTask task = professionService.getTemplateInitTask();
+        if ( task != null ) {
+            setAttr( "LOGS", task.getLogs() );
+            setAttr( "STATUS", task.status() );
+        }
+    }
+
+    public void finish() {
+        IdentityContext context = IdentityContext.getIdentityContext( this );
+        ProfessionService professionService = context.getEnterpriseService().getProfessionService();
+        TemplateInitTask task = professionService.getTemplateInitTask();
+        if ( task != null && task.status() == TemplateInitializerState.FINISH ) {
+            professionService.finishInitTemplate();
+            redirect( "/" );
+        }
+    }
+
+    private boolean setProfession( Profession profession ) {
+        IdentityContext context = IdentityContext.getIdentityContext( this );
+        EnterpriseService enterpriseService = context.getEnterpriseService();
+        enterpriseService.initProfessionService( profession );
+        ProfessionService professionService = enterpriseService.getProfessionService();
+        // 一年内不过期
         setCookie( "profession", String.valueOf( profession.getId() ), 365 * 24 * 60 * 60 );
+        // 如果模板未初始化则进行初始化
+        if ( professionService.templateInitialized() == false ) {
+            professionService.initTemplate( context.getIdentity() );
+            redirect( "/e/status#this" );
+            return false;
+        }
+        return true;
     }
 }
